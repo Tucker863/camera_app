bea15c80122a0a84f0598a382ecf366ea4abab3f
Tucker <munkhtsooj_turtogtokh@wagnerequipment.com>
2020-08-28 14:25:01 -0600

    api list detail done on other categories from bottom  navigation bar

--- lib/Screens/APIList.dart
+++ lib/Screens/APIList.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
+import 'package:camreatest/Models/Book.dart';
 import 'package:camreatest/Screens/ListItem.dart';
 import 'package:camreatest/Utils/API.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'package:flappy_search_bar/flappy_search_bar.dart';
+import 'package:flappy_search_bar/scaled_tile.dart';
 
 class APIList extends StatefulWidget {
   @override
@@ -9,13 +13,13 @@ class APIList extends StatefulWidget {
 }
 
 class _APIListState extends State<APIList> {
-  var products = new List<ProductMax>();
+  var products = new List<Book>();
 
   _getUsers() {
-    API.getProductsFromApi().then((response) {
+    API.getAllBooksFromApi().then((response) {
       setState(() {
         Iterable list = json.decode(response.body);
-        products = list.map((model) => ProductMax.fromJson(model)).toList();
+        products = list.map((model) => Book.fromJson(model)).toList();
       });
     });
   }
@@ -31,9 +35,9 @@ class _APIListState extends State<APIList> {
 
   String _value = 'Hello World';
 
-  void _onPressed(ProductMax product, String appBarTitle) {
+  void _onPressed(Book book, String appBarTitle) {
     Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return ListItem(product, appBarTitle);
+      return ListItem(book, appBarTitle);
     }));
   }
 
@@ -49,47 +53,9 @@ class _APIListState extends State<APIList> {
             return ListTile(
                 title: RaisedButton(
                     onPressed: () =>
-                        _onPressed(products[index], products[index].name),
-                    child: new Text(products[index].name)));
+                        _onPressed(products[index], products[index].title),
+                    child: new Text(products[index].title)));
           },
         ));
-  }
-}
-
-class ProductMax {
-  int id;
-  String name;
-  String description;
-  double price;
-  int category_id;
-  String category_name;
-
-  ProductMax(int id, String name, String description, double price,
-      int category_id, String category_name) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.price = price;
-    this.category_id = category_id;
-    this.category_name = category_name;
-  }
-
-  ProductMax.fromJson(Map json)
-      : id = int.parse(json['id']),
-        name = json['name'],
-        description = json['description'],
-        price = double.parse(json['price']),
-        category_id = int.parse(json['category_id']),
-        category_name = json['category_name'];
-
-  Map toJson() {
-    return {
-      'id': id,
-      'name': name,
-      'description': description,
-      'price': price,
-      'category_id': category_id,
-      'category_name': category_name
-    };
   }
 }
